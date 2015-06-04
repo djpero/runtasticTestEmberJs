da3a132884ae1440622b03e8e8caaf40d7207a0a
Pero Knezevic <djpero.84@gmail.com>
2015-06-04 22:45:49 +0200

    Removing counter

--- app/routes/sessions.js
+++ app/routes/sessions.js
@@ -6,13 +6,12 @@ export default Ember.Route.extend({
         
   		//var store = this.get('store');
   		var store = [];
-  		var i=1;
+  		
   		console.log(param);
         return Ember.$.getJSON('http://intense-bastion-3210.herokuapp.com?page='+param['page']).then(function(response) {
             if (response) {
                 response.run_sessions.forEach(function(datas) {
                     store.push({sessionId: datas.id, startAt: datas.start_time, endAt: datas.end_time, duration: datas.duration, distance: datas.distance, typeId:datas.sport_type_id, encodedTrace: datas.encoded_trace});
-                    i++;
                 });        		
             }
             
