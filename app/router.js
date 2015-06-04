be17a4a7d6b86235eb4783cc7adcba79d7a67bf8
Pero Knezevic <djpero.84@gmail.com>
2015-06-04 22:41:32 +0200

    Fix :) path route of sessions

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('rdata');
   this.resource('rdata', { path: '/' });
   this.route('sessions');
+  this.resource('sessions', {path: '/sessions'});
 });
 
 export default Router;
