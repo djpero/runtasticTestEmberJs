02fa7bab165a2fed185841e3c8b6405ec3a81ac2
Pero Knezevic <djpero.84@gmail.com>
2015-06-04 23:15:21 +0200

    formatting date

--- app/routes/sessions.js
+++ app/routes/sessions.js
@@ -9,7 +9,9 @@ export default Ember.Route.extend({
         return Ember.$.getJSON('http://intense-bastion-3210.herokuapp.com?page='+param['page']+'&sort_by='+param['sort_by']+'&order='+param['order']).then(function(response) {
             if (response) {
                 response.run_sessions.forEach(function(datas) {
-                    store.push({sessionId: datas.id, startAt: datas.start_time, endAt: datas.end_time, duration: datas.duration, distance: datas.distance, typeId:datas.sport_type_id, encodedTrace: datas.encoded_trace});
+                	var start_time = new Date(datas.start_time);
+                	var end_time = new Date(datas.end_time);
+                    store.push({sessionId: datas.id, startAt: formatDate(start_time), endAt: formatDate(end_time), duration: datas.duration, distance: datas.distance, typeId:datas.sport_type_id, encodedTrace: datas.encoded_trace});
                 });        		
             }
             
@@ -20,3 +22,16 @@ export default Ember.Route.extend({
       controller.set('list', model);
     }
 });
+function formatDate(date){
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var day = date.getDate();
+	var month = date.getMonth()+1;
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	hours = hours < 10 ? '0'+hours : hours;
+	day = day < 10 ? '0'+day : day;
+	month = month < 10 ? '0'+month : month;
+	var strTime = hours + '.' + minutes
+	return  day+ "." + month  + "." + date.getFullYear() + ", " + strTime;
+
+}	
