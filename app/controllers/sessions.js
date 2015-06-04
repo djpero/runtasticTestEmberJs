2b46b974f849a209c85d28fbb26e524ff4c2d205
Pero Knezevic <djpero.84@gmail.com>
2015-06-04 22:44:57 +0200

    Adding params for sorting and paging in Controller sessions Copying code from route rdata, to accept params in JSON calls to backend herokuapp

--- app/controllers/sessions.js
+++ app/controllers/sessions.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	queryParams: ['page', 'sort_by', 'order'],
+	page: null,
+	sort_by: null,
+	order: null,
 });
--- app/routes/sessions.js
+++ app/routes/sessions.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  	model: function(param) {
+        
+  		//var store = this.get('store');
+  		var store = [];
+  		var i=1;
+  		console.log(param);
+        return Ember.$.getJSON('http://intense-bastion-3210.herokuapp.com?page='+param['page']).then(function(response) {
+            if (response) {
+                response.run_sessions.forEach(function(datas) {
+                    store.push({sessionId: datas.id, startAt: datas.start_time, endAt: datas.end_time, duration: datas.duration, distance: datas.distance, typeId:datas.sport_type_id, encodedTrace: datas.encoded_trace});
+                    i++;
+                });        		
+            }
+            
+           return store;
+        });
+    },
+    setupController: function(controller,model) {
+      controller.set('list', model);
+    }
 });
